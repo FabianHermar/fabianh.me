2ce15d1fe011aa255b6e695d1c3b08cfa0df0aed
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-13 13:34:58 -0600

    refactor: Update tailwind.config.mjs with new theme colors and container settings

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,100 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+module.exports = {
+  darkMode: [ "class" ],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
-        GilroyRegular: ['GilroyRegular', 'sans-serif'],
-        GilroyBold: ['GilroyBold', 'sans-serif'],
-        GilroySemibold: ['GilroySemibold', 'sans-serif']
+        GilroyRegular: [ 'GilroyRegular', 'sans-serif' ],
+        GilroyBold: [ 'GilroyBold', 'sans-serif' ],
+        GilroySemibold: [ 'GilroySemibold', 'sans-serif' ]
       }
-    }
+    },
+    colors: {
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
+      neutral: {
+        50: "#fafafa",
+        100: "#f5f5f5",
+        200: "#e5e5e5;",
+        300: "#d4d4d4",
+        400: "#a3a3a3",
+        500: "#737373",
+        600: "#525252",
+        700: "#404040",
+        800: "#262626",
+        900: "#171717",
+        950: "#0a0a0a",
+      },
+      black: "rgb(0,0,0)",
+      white: "rgb(255,255,255)",
+      transparent: "transparent",
+      primary: {
+        DEFAULT: "hsl(var(--primary))",
+        foreground: "hsl(var(--primary-foreground))",
+      },
+      secondary: {
+        DEFAULT: "hsl(var(--secondary))",
+        foreground: "hsl(var(--secondary-foreground))",
+      },
+      destructive: {
+        DEFAULT: "hsl(var(--destructive))",
+        foreground: "hsl(var(--destructive-foreground))",
+      },
+      muted: {
+        DEFAULT: "hsl(var(--muted))",
+        foreground: "hsl(var(--muted-foreground))",
+      },
+      accent: {
+        DEFAULT: "hsl(var(--accent))",
+        foreground: "hsl(var(--accent-foreground))",
+      },
+      popover: {
+        DEFAULT: "hsl(var(--popover))",
+        foreground: "hsl(var(--popover-foreground))",
+      },
+      card: {
+        DEFAULT: "hsl(var(--card))",
+        foreground: "hsl(var(--card-foreground))",
+      },
+    },
+    borderRadius: {
+      lg: "var(--radius)",
+      md: "calc(var(--radius) - 2px)",
+      sm: "calc(var(--radius) - 4px)",
+      xs: "calc(var(--radius) - 6px)",
+      xl: "calc(var(--radius) + 2px)",
+      "2xl": "calc(var(--radius) + 4px)",
+      "3xl": "calc(var(--radius) + 6px)",
+      full: "9999px",
+    },
+    keyframes: {
+      "accordion-down": {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
   },
-  plugins: []
+  plugins: [ require( "tailwindcss-animate" ) ],
 }
