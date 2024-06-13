34bf74e924892467fa019e5d5566eac43983c1de
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-13 13:33:49 -0600

    refactor: Update astro.config.mjs to include react integration and disable base styles in tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [ tailwind() ],
+  integrations: [ tailwind( {
+    applyBaseStyles: false,
+  } ), react() ],
   i18n: {
     defaultLocale: 'es',
     locales: [ 'es', 'en', 'de' ],
