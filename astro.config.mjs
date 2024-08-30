bf555ac8cc3f66c1686a01598b668ab5efd549ed
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-08-30 02:17:07 -0600

    refactor: Update Astro config with optimized tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
-
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [ tailwind( {
+  integrations: [tailwind({
     applyBaseStyles: false
-  } ), react() ],
+  }), react()],
   i18n: {
     defaultLocale: 'es',
-    locales: [ 'es', 'en', 'de' ],
+    locales: ['es', 'en', 'de'],
     routing: {
       prefixDefaultLocale: false
     }
   }
-} )
+})
