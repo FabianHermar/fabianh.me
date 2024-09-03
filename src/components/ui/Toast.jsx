2e3514062e6805c9c3bcb315c3c98f4601779427
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-09-03 14:05:58 -0600

    refactor: Update Toast component to use useEffect for displaying notification

--- src/components/ui/Toast.jsx
+++ src/components/ui/Toast.jsx
@@ -1,12 +1,21 @@
 import { Toaster, toast } from 'sonner'
+import { useEffect } from 'react'
 
-function Toast ({ message }) {
+function Notification () {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      toast.info('The website is not yet complete, this is only a beta version of the final design. The final version will be ready soon.', {
+        duration: 10000
+      })
+    }, 1000)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <>
-      <Toaster />
-      <button onClick={() => toast.info(message)} className='bg-neutral-400 h-4 w-4'></button>
+      <Toaster position='bottom-right' closeButton />
     </>
   )
 }
 
-export default Toast
+export default Notification
