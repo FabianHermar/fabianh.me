35d04ee424b11651b946c8cada0381e3098d9be7
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-11-15 17:40:37 -0600

    fix: Remove service worker registration and implementation files

--- src/registerServiceWorker.js
+++
@@ -1,9 +0,0 @@
-if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then((registration) => {
-      console.log('Service Worker registrado con éxito:', registration);
-    })
-    .catch((error) => {
-      console.log('Error al registrar el Service Worker:', error);
-    });
-}
