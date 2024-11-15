ff17546196234ebf2c45ceb908d2b72b10495f34
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-11-15 17:40:45 -0600

    feat: Add service worker for caching static assets

---
+++ public/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'cache-v1';
+const STATIC_ASSETS = [
+  '/',
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching static assets');
+      return cache.addAll(STATIC_ASSETS).catch((error) => {
+        console.error('Failed to cache:', error);
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open(CACHE_NAME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
