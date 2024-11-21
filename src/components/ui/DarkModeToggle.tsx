75f97a74edaa57f8509e8f0e4539e22784f33993
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-11-21 12:18:25 -0600

    fix: Refactor DarkModeToggle component for improved formatting and consistency

--- src/components/ui/DarkModeToggle.tsx
+++ src/components/ui/DarkModeToggle.tsx
@@ -3,10 +3,10 @@ import * as React from 'react'
 
 import { Button } from '@/components/ui/button.tsx'
 import {
-  DropdownMenu,
-  DropdownMenuContent,
+	DropdownMenu,
+	DropdownMenuContent,
 	DropdownMenuItem,
-	DropdownMenuTrigger
+	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu.tsx'
 
 export function ModeToggle() {
