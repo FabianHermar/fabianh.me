3c267fb13fd545ab136c48c3554bb24b0eff11c0
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-11-26 01:18:52 -0600

    feat: Add TypeScript definitions for environment variables in ImportMeta

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_IPINFO_API_KEY: string
+  readonly VITE_OPENWEATHER_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
