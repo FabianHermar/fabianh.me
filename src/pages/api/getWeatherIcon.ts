be473d2e3b083f53c680e747ef248c79f3f1c638
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-11-27 00:04:08 -0600

    feat: Enhance getWeatherIcon function to include cloud coverage handling for improved icon selection

--- src/pages/api/getWeatherIcon.ts
+++ src/pages/api/getWeatherIcon.ts
@@ -1,40 +1,45 @@
 import type { WeatherData } from '@/types/weather'
 
-export const getWeatherIcon = (weather: WeatherData, currentTime: number):string => {
-  if (!weather) return ""
-  
-  const isDaytime = currentTime >= 6 && currentTime < 18
-  const condition = weather.weather[0].main.toLowerCase()
+export const getWeatherIcon = (weather: WeatherData, currentTime: number): string => {
+	if (!weather) return ''
 
-  switch (condition) {
-    case "clear":
-      return isDaytime ? "clear-day.svg" : "clear-night.svg";
-    
-    case "clouds":
-      return 'cloudy.svg';
+	const isDaytime = currentTime >= 6 && currentTime < 18
+	const condition = weather.weather[0].main.toLowerCase()
+	const clouds = weather.clouds.all
 
-    case "rain":
-      return 'rain.svg';
+	switch (condition) {
+		case 'clear':
+			return isDaytime ? 'clear-day.svg' : 'clear-night.svg'
 
-    case "drizzle":
-      return 'drizzle.svg';
+		case 'clouds':
+			if (clouds > 85) {
+				return isDaytime ? 'overcast-day.svg' : 'overcast-night.svg'
+			} else {
+				return isDaytime ? 'partly-cloudy-day.svg' : 'partly-cloudy-night.svg'
+			}
 
-    case "thunderstorm":
-      return 'thunderstorm.svg';
-    
-    case "snow":
-      return 'snow.svg';
-    
-    case "fog":
-      return 'fog.svg';
+		case 'rain':
+			return 'rain.svg'
 
-    case "sleet":
-      return 'sleet.svg';
+		case 'drizzle':
+			return 'drizzle.svg'
 
-    case "tornado":
-      return 'tornado.svg';
+		case 'thunderstorm':
+			return 'thunderstorm.svg'
 
-    default:
-      return "clear-day.svg";
-  }
+		case 'snow':
+			return 'snow.svg'
+
+		case 'fog':
+			return 'fog.svg'
+
+		case 'sleet':
+			return 'sleet.svg'
+
+		case 'tornado':
+			return 'tornado.svg'
+
+		default:
+			return 'clear-day.svg'
+	}
 }
