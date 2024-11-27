974b721434b5554e40f226e6b17617ef4faa29ce
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-11-27 00:04:23 -0600

    feat: Extend WeatherData interface to include additional properties for improved weather data representation

--- src/types/weather.d.ts
+++ src/types/weather.d.ts
@@ -1,18 +1,24 @@
 export interface WeatherData {
-  main: {
-    temp: number
-    humedity: number
-  }
-  weather: {
-    main: string
-    description: string
-  }[]
-  wind: {
-    speed: number
-  }
+	main: {
+		temp: number
+		humidity: number
+		temp_min: number
+		temp_max: number
+	}
+	visibility: number
+	weather: {
+		main: string
+		description: string
+	}[]
+	wind: {
+		speed: number
+	}
+	clouds: {
+		all: number
+	}
 }
 
 export interface LocationData {
-  city: string
-  loc: string
+	city: string
+	loc: string
 }
