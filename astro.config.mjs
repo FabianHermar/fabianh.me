f727426c090e019cb40062c81723dd99421a24f5
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-11-27 00:03:12 -0600

    feat: Add environment variable schema for IPINFO and OpenWeather API keys in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,43 @@
 import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react()],
+	experimental: {
+		env: {
+			schema: {
+				IPINFO_API_KEY: envField.string({ context: 'client', access: 'public' }),
+				OPENWEATHER_API_KEY: envField.string({ context: 'client', access: 'public' }),
+			},
+		},
+	},
 
-  i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en', 'de'],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  },
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+	],
 
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+	i18n: {
+		defaultLocale: 'es',
+		locales: ['es', 'en', 'de'],
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
 
-  vite: {
-    assetsInclude: ['/sw.js'],
-    envDir: '/.env',
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
+
+	vite: {
+		assetsInclude: ['/sw.js'],
 		ssr: {
-			noExternal: ["path-to-regexp"],
-		}
-  }
+			noExternal: ['path-to-regexp'],
+		},
+	},
 })
