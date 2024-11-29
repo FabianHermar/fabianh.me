6a0e2b521735944b283a669b1543f7e90b774c53
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-11-29 16:37:26 -0600

    feat: Add AUTH_TOKEN to environment variables in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig({
 				IPINFO_API_KEY: envField.string({ context: 'client', access: 'public' }),
 				OPENWEATHER_API_KEY: envField.string({ context: 'client', access: 'public' }),
 				RESEND_API_KEY: envField.string({ context: 'client', access: 'public' }),
+				AUTH_TOKEN: envField.string({ context: 'client', access: 'public' }),
 			},
 		},
 	},
