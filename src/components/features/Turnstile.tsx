105a4e9147b41c5fd8470e31c08591d75e3e1def
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-12-01 12:08:55 -0600

    fix: Improve formatting and readability in Turnstile component

--- src/components/features/Turnstile.tsx
+++ src/components/features/Turnstile.tsx
@@ -1,35 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 
 interface TurnstileComponentProps {
+	onVerify: (token: string) => void
 
-  onVerify: (token: string) => void;
-
-  onError: () => void;
-
+	onError: () => void
 }
 
 const TurnstileComponent: React.FC<TurnstileComponentProps> = ({ onVerify, onError }) => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src =
-      'https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback';
-    script.defer = true;
-    document.body.appendChild(script);
+	useEffect(() => {
+		const script = document.createElement('script')
+		script.src =
+			'https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback'
+		script.defer = true
+		document.body.appendChild(script)
 
-    return () => {
-      // Limpieza: elimina el script al desmontar el componente
-      document.body.removeChild(script);
-    };
-  }, []);
+		return () => {
+			// Limpieza: elimina el script al desmontar el componente
+			document.body.removeChild(script)
+		}
+	}, [])
 
-  return (
-    <div
-      className="cf-turnstile"
-      data-sitekey="0x4AAAAAAA0MPnnImP_nrb-h"
-      data-callback="javascriptCallback"
-      data-theme="light"
-    />
-  );
-};
+	return (
+		<div
+			className='cf-turnstile'
+			data-sitekey='0x4AAAAAAA0MPnnImP_nrb-h'
+			data-callback='javascriptCallback'
+			data-theme='light'
+		/>
+	)
+}
 
-export default TurnstileComponent;
+export default TurnstileComponent
