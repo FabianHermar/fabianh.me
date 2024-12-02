f3e7329ce76383a17342e960cf4a069cbf9eae35
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-12-02 17:37:36 -0600

    fix: Refactor service worker to improve caching logic and logging

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache-v1';
+const CACHE_NAME = 'cache-v1'
 const STATIC_ASSETS = [
 	'/',
 	'/index.html',
@@ -17,48 +17,50 @@ const STATIC_ASSETS = [
 ]
 
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching static assets');
-      return cache.addAll(STATIC_ASSETS).catch((error) => {
-        console.error('Failed to cache:', error);
-      });
-    })
-  );
-});
+	console.log('Service Worker installing.')
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			console.log('Caching static assets')
+			return cache.addAll(STATIC_ASSETS).catch((error) => {
+				console.error('Failed to cache:', error)
+			})
+		})
+	)
+})
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating.');
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+	console.log('Service Worker activating.')
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if (cacheName !== CACHE_NAME) {
+						console.log('Deleting old cache:', cacheName)
+						return caches.delete(cacheName)
+					}
+				})
+			)
+		})
+	)
+})
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return caches.open(CACHE_NAME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
-});
+	if (event.request.url.startsWith(self.location.origin)) {
+		event.respondWith(
+			caches.match(event.request).then((cachedResponse) => {
+				if (cachedResponse) {
+					return cachedResponse
+				}
+				return caches.open(CACHE_NAME).then((cache) => {
+					return fetch(event.request).then((response) => {
+						// Solo almacenar en caché las solicitudes GET
+						if (event.request.method === 'GET') {
+							cache.put(event.request, response.clone())
+						}
+						return response
+					})
+				})
+			})
+		)
+	}
+})
