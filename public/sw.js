ebf33334dbd0929d3b783c53e066140991e7c4c7
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-12-02 17:33:03 -0600

    fix: Update service worker cache to include additional assets

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,20 @@
 const CACHE_NAME = 'cache-v1';
 const STATIC_ASSETS = [
-  '/',
-  '/index.html',
-  '/favicon/favicon.ico',
-  '/FabianHerrera_CV.pdf',
-  '/me.avif',
-  '/portfolio_mockup.webp',
-  '/src/globals.css',
-  '/src/layouts/Layout.astro',
-  '/src/pages/index.astro',
-  '/src/pages/guia-de-estilo.astro',
-  '/src/pages/404.astro',
-  '/src/pages/en/index.astro',
-  '/src/pages/en/guide-style.astro',
-  '/src/pages/de/index.astro',
-  '/src/pages/de/stilhandbuch.astro',
-  '/src/styles/Bento.css',
-  '/src/styles/DownloadButton.module.css',
-];
+	'/',
+	'/index.html',
+	'/favicon/favicon.ico',
+	'/FabianHerrera_CV.pdf',
+	'/me.avif',
+	'/portfolio_mockup.webp',
+	'/src/globals.css',
+	'/src/styles/Bento.css',
+	'/src/styles/DownloadButton.module.css',
+	'/projects/DuneUI.webp',
+	'/projects/UTNA.webp',
+	'/fonts/Gilroy-Bold.woff2',
+	'/fonts/Gilroy-SemiBold.woff2',
+	'/fonts/Gilroy-Regular.woff2',
+]
 
 self.addEventListener('install', (event) => {
   console.log('Service Worker installing.');
