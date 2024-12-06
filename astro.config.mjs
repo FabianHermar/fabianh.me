8fb5bba1a50144384d569b2f42c19bfbc5b724be
Fabian Herrera <contactofabianmartinez@gmail.com>
2024-12-06 00:32:44 -0600

    fix: Update Astro configuration for improved serverless deployment and environment variable handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,20 @@
 import tailwind from '@astrojs/tailwind'
 import { defineConfig, envField } from 'astro/config'
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
-	experimental: {
-		env: {
-			schema: {
-				IPINFO_API_KEY: envField.string({ context: 'client', access: 'public' }),
-				OPENWEATHER_API_KEY: envField.string({ context: 'client', access: 'public' }),
-				RESEND_API_KEY: envField.string({ context: 'client', access: 'public' }),
-				AUTH_TOKEN: envField.string({ context: 'client', access: 'public' }),
-				SITE_KEY: envField.string({ context: 'client', access: 'public' }),
-			},
+	env: {
+		schema: {
+			IPINFO_API_KEY: envField.string({ context: 'client', access: 'public' }),
+			OPENWEATHER_API_KEY: envField.string({ context: 'client', access: 'public' }),
+			RESEND_API_KEY: envField.string({ context: 'client', access: 'public' }),
+			AUTH_TOKEN: envField.string({ context: 'client', access: 'public' }),
+			SITE_KEY: envField.string({ context: 'client', access: 'public' }),
 		},
-		serverIslands: true,
 	},
-
-	publicDir: 'public',
+	serverIslands: true,
 
 	integrations: [
 		tailwind({
