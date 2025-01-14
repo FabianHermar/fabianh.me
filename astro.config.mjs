8f5143aa3aff75a95a2e11d51bf09901e2ab21de
Fabian Herrera <contactofabianmartinez@gmail.com>
2025-01-13 21:32:25 -0600

    feat: Update Astro configuration for improved environment handling and add Partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,41 +3,45 @@ import { defineConfig, envField } from 'astro/config'
 import react from '@astrojs/react'
 import vercel from '@astrojs/vercel'
 
+import partytown from '@astrojs/partytown'
+
 // https://astro.build/config
 export default defineConfig({
-    env: {
-        schema: {
-            IPINFO_API_KEY: envField.string({ context: 'client', access: 'public' }),
-            OPENWEATHER_API_KEY: envField.string({ context: 'client', access: 'public' }),
-            RESEND_API_KEY: envField.string({ context: 'client', access: 'public' }),
-            AUTH_TOKEN: envField.string({ context: 'client', access: 'public' }),
-            SITE_KEY: envField.string({ context: 'client', access: 'public' }),
-        },
-        validateSecrets: true,
-    },
-    serverIslands: true,
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
+	env: {
+		schema: {
+			IPINFO_API_KEY: envField.string({ context: 'server', access: 'secret' }),
+			OPENWEATHER_API_KEY: envField.string({ context: 'server', access: 'secret' }),
+			RESEND_API_KEY: envField.string({ context: 'server', access: 'secret' }),
+			AUTH_TOKEN: envField.string({ context: 'server', access: 'secret' }),
+			SITE_KEY: envField.string({ context: 'server', access: 'public' }),
+		},
+		validateSecrets: true,
+	},
+	serverIslands: true,
 
-    integrations: [tailwind({
-        applyBaseStyles: false,
-		}), react()],
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+		partytown(),
+	],
 
-    i18n: {
-        defaultLocale: 'es',
-        locales: ['es', 'en', 'de'],
-        routing: {
-            prefixDefaultLocale: false,
-        },
-    },
+	i18n: {
+		defaultLocale: 'es',
+		locales: ['es', 'en', 'de'],
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
 
-    output: 'server',
-    adapter: vercel({
-        webAnalytics: { enabled: true },
-    }),
-
-    vite: {
-        assetsInclude: ['/sw.js'],
-        ssr: {
-            noExternal: ['path-to-regexp'],
-        },
-    },
+	vite: {
+		ssr: {
+			noExternal: ['path-to-regexp'],
+		},
+	},
 })
